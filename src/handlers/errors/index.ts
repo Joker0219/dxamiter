01c99da9c507ceb39492fbd2ace7bbed7e1e6981
295360998@qq.com <295360998@qq.com>
2022-03-24 12:27:27 +0800

    Add Blank Srceen

--- src/handlers/errors/index.ts
+++ src/handlers/errors/index.ts
@@ -1,8 +1,10 @@
+import { injectBlankScreen } from "./blankScreen";
 import { injectError } from "./jserror";
 import { injectPromise } from "./promise";
 export function handleErrors(): ErrorHandlers {
     return {
         jserror: injectError,
         promise: injectPromise,
+        blankScreen: injectBlankScreen
     }
 }
